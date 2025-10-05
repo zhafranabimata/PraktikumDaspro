a2bdf613874465f3afeed20d80ee1bd70d84a459
Zhafranabimata <adyaganteng981@gmail.com>
2025-10-05 21:55:37 +0700

    tugas3 part 2

---
+++ jobsheet5/latihan30part2.java
@@ -0,0 +1,34 @@
+package PraktikumDaspro.jobsheet5;
+
+import java.util.Scanner;
+
+public class latihan30part2 {
+    public static void main(String[] args) {
+        String user;
+        int nilai;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Anda sebagai apa? (Dosen/Mahasiswa): ");
+        user = input.nextLine();
+
+        System.out.println("==============================");
+
+        if (user.equalsIgnoreCase("dosen")) {
+            System.out.println("Anda diberikan akses wifi (dosen)");
+        } else if (user.equalsIgnoreCase("mahasiswa")) {
+            System.out.print("Input SKS anda: ");
+            nilai = input.nextInt();
+
+            if (nilai >= 12) {
+                System.out.println("Anda diberikan akses wifi (mahasiswa)");
+            } else {
+                System.out.println("Akses wifi ditolak");
+            }
+        } else {
+            System.out.println("Akses ditolak");
+        }
+
+        input.close();
+    }
+}
