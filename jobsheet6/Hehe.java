a7040924c1a3899f56a86dec1c7a30d00d3a08ec
Zhafranabimata <adyaganteng981@gmail.com>
2025-10-09 09:36:13 +0700

    baru

---
+++ jobsheet6/Hehe.java
@@ -0,0 +1,21 @@
+package PraktikumDaspro.jobsheet6;
+import java.util.Scanner;
+
+public class Hehe {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    
+        int nilai, huruf;
+        boolean nilaiakhir;
+        String nama;
+        
+        System.out.println("Nama Mahasiswa");
+        nama = sc.next();
+
+
+
+    }
+
+ }
+    
